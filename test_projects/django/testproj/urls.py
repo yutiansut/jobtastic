674001236e3eda19f56c8416503a7951f1f9a0af
yutiansut <yutiansut@qq.com>
2019-09-16 03:45:47 +0800

    #fix url error for django update

--- test_projects/django/testproj/urls.py
+++ test_projects/django/testproj/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 try:
     from djcelery.views import apply
-    urlpatterns = patterns('',
+    urlpatterns = [
         url(r'^apply/(?P<task_name>.+?)/', apply, name='celery-apply'),
         url(r'^celery/', include('djcelery.urls')),
-    )
+    ]
 except ImportError:
-    urlpatterns = patterns('')
+    urlpatterns = []
